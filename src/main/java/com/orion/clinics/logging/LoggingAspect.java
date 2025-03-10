7154b2fe43e26e8b2cc8161bb1bff1122023ae31
Karolina Andrulyte <karolina.andrulyte@gmail.com>
2025-03-10 12:37:47 +0200

    Added AOP logging for services

---
+++ src/main/java/com/orion/clinics/logging/LoggingAspect.java
@@ -0,0 +1,31 @@
+package com.orion.clinics.logging;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Aspect
+@Component
+@Slf4j
+public class LoggingAspect {
+
+    @Pointcut("execution(public * com.orion.clinics.services.*.*(..))")
+    private void publicMethodsFromServicesPackage() {}
+
+    @Around(value = "publicMethodsFromServicesPackage()")
+    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        Object[] args = joinPoint.getArgs();
+        String methodName = joinPoint.getSignature().getName();
+
+        log.debug(">> {}() - {}", methodName, Arrays.toString(args));
+        Object result = joinPoint.proceed();
+
+        log.debug("<< {}() - {}", methodName, result);
+        return result;
+    }
+}
